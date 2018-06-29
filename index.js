27485d8653e90aceccc4eb00aa01c5c2c480f7a7
cayetanomarco@yahoo.com <cayetanomarco@yahoo.com>
2018-06-29 07:54:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,12 +14,14 @@ function sayHiToGrandma(string){
   if(string.toLowerCase===string){
     return "I can't hear you!";
   }
-  else if(string==="I love you, Grandma"){
-    return "I love you, too";
-  }
 }
 function sayHiToGrandma(string){
  if(string.toUpperCase===string){
     return "YES INDEED";
+  }
+}
+function sayHiToGrandma(string){
+ if(string==="I love you, Grandma"){
+    return "I love you, too";
   }
 }
