5dbc8edda2292ad0b61c1a321f1e3ce1644a1476
cayetanomarco@yahoo.com <cayetanomarco@yahoo.com>
2018-06-29 07:48:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ function shout(string){
 function whisper(string){
   return srting.toLowerCase();
 }
+function logshout(string){
+  console.log(string.toUpperCase());
+}
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+function sayHiToGrandma(string){
+  if(string.toLowerCase===true){
+    return "I can't hear you!";
+  }
+  else if(string.toUpperCase===true){
+    return "YES INDEED";
+  }
+  else if(string==="I love you, Grandma"){
+    return "I love you, too";
+  }
+}
